e37ddfd1313a4439b5f3ff0f3beb47635bcb8530
diegonacif <diegoreisnacif@gmail.com>
2023-06-05 16:58:42 -0300

    feat: hello component parallax

--- src/components/Hello/Hello.jsx
+++ src/components/Hello/Hello.jsx
@@ -7,18 +7,39 @@ export const Hello = () => {
   return (
     <div className="hello-container">
       <section className="hello-img">
-        <img src={blob1} alt="" id="blob1" />
-        <img src={diegoImg} alt="diego image" id="diego-img" />
+        <Parallax 
+          translateY={['200px', '200px']}
+          opacity={[1, 0]}
+          shouldAlwaysCompleteAnimation={true}
+          style={{ zIndex: 10 }}
+        >
+          <img src={blob1} alt="" id="blob1" />
+          <img src={diegoImg} alt="diego image" id="diego-img" />
+        </Parallax>
+        {/* <Parallax 
+          translateY={['120px', '150px']}
+          opacity={[0, 0.8]}
+          shouldAlwaysCompleteAnimation={true}
+          style={{ zIndex: 10 }}
+        >
+          <div className="gradient-overlay"></div>
+        </Parallax> */}
       </section>
       <section className="hello-text">
-        <Parallax translateX={[-65, 100]}>
+        <Parallax translateX={[-11, 20]}>
           <span>Olá, meu nome é</span>
         </Parallax>
         <div className="my-name" translate='no'>
-          <h2>Diego&nbsp;</h2>
-          <h2>Nacif</h2>
+          <Parallax translateX={[11, -20]}>
+            <h2>Diego&nbsp;</h2>
+          </Parallax>
+          <Parallax translateX={[-11, 20]}>
+            <h2>Nacif</h2>
+          </Parallax>
         </div>
-        <span>Desenvolvedor <b>Front-End</b></span>
+        <Parallax translateX={[11, -20]}>
+          <span>Desenvolvedor <b>Front-End</b></span>
+        </Parallax>
       </section>
       <section className="hello-buttons">
         <button>DOWNLOAD CV</button>
