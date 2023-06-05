eb39cdb0e3ceeb2a37f304f4bc16b1e80f3cd400
diegonacif <diegoreisnacif@gmail.com>
2023-06-05 11:49:52 -0300

    feat: app container scroll snap

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.scss'
+import { AboutMe } from './components/AboutMe/AboutMe'
 import { Hello } from './components/Hello/Hello'
 
 export const App = () => {
 
   return (
-    <Hello />
+    <main className="app-container">
+      <Hello />
+      <AboutMe />
+    </main>
   )
 }
 
---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,9 @@
+import '../../App.scss';
+
+export const AboutMe = () => {
+  return (
+    <div className="about-me-container">
+      <h1>About Me</h1>
+    </div>
+  )
+}
