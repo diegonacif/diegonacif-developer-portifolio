8381806c9bb1a4495ffc236dfbe7b68c989a45c0
diegonacif <diegoreisnacif@gmail.com>
2023-06-07 09:43:16 -0300

    get screenshot function

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,20 +1,75 @@
 import { Parallax } from "react-scroll-parallax"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import '../../App.scss';
 import api from "../../services/api";
 
 export const Projects = () => {
+  const [imageData, setImageData] = useState([]);
+  const [repositories, setRepositories] = useState([]);
+  const [loading, setIsLoading] = useState(true);
+
+  // console.log(imageData);
+
   useEffect(() => {
-    async function getRepository() {
-      await api
-        .get(`/repos?per_page=100`)
-        .then((response) => console.log(response.data[8].contents_url))
-        .catch((err) => {
-          console.error("ops! ocorreu um erro" + err);
-        });
-      }      
-      getRepository();
-    }, []);
+    repositories.length > 0 ? setIsLoading(false) : setIsLoading(true);
+  }, [repositories])
+
+  useEffect(() => {
+    const getRepositories = async () => {
+      try {
+        const response = await api.get("/repos?per_page=100");
+        setRepositories(response.data);
+      } catch (error) {
+        console.error("Ops! Ocorreu um erro:", error);
+      }
+    };
+
+    getRepositories();
+  }, []);
+
+  useEffect(() => {
+    if( loading === true) {
+      console.log("loading")
+    } else {
+      const fetchImagesFromRepositories = async () => {
+        try {
+          const imageUrls = [];
+    
+          for (let i = 0; i < repositories.length; i++) {
+            const contentsUrl = repositories[i]?.contents_url;
+            const response = await api.get(contentsUrl?.replace("{+path}", ""));
+            const contents = response.data;
+            const contentsArray = Array.isArray(contents) ? contents : [contents];
+    
+            const imageFiles = contentsArray.filter((file) => {
+              const extensions = [".png", ".jpg", ".jpeg", ".gif"];
+              const fileExtension = file.name.slice(file.name.lastIndexOf(".")).toLowerCase();
+              return extensions.includes(fileExtension);
+            });
+
+            for (const image of imageFiles) {
+              imageUrls.push({ url: image.download_url, repositoryId: repositories[i].id });
+            }
+          }
+          setImageData(imageUrls);
+        } catch (error) {
+          console.error("Ocorreu um erro ao recuperar as imagens:", error);
+        }
+      };
+      fetchImagesFromRepositories();
+    }
+  }, [loading]);
+
+  const getScreenshot = (id) => {
+    const screenshotObject = imageData.find((item) => item.repositoryId === id);
+    return screenshotObject ? screenshotObject.url : null;
+    // return console.log(screenshotObject.url)
+  };
+
+  getScreenshot(614936534);
+
+  // console.log(getScreenshot(614936534))
+
   return (
     <section className="projects-container">
       <div className="projects-title">
@@ -43,6 +98,8 @@ export const Projects = () => {
           <span>&gt;</span>
         </Parallax>
       </div>
+
+      <img src={() => getScreenshot(614936534)} alt="" />
     </section>
   )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 
+const accessToken = "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";
+
 const api = axios.create({
   baseURL: "https://api.github.com/users/diegonacif",
   client_id: "d3edd6cd01933a54bd7d",
-  client_secret: "766d6eb58ec8025ef8e0c065f7d068de2f0bf3f0"
+  client_secret: "766d6eb58ec8025ef8e0c065f7d068de2f0bf3f0",
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+    // "Content-Type": "application/json",
+  },
 });
 
 export default api;
